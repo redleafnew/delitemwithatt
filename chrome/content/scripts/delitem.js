ccf9781585b318788fa04ee7c376ad04d1b79c83
redleafnew <redleafnew@163.com>
2021-04-30 17:36:34 +0800

    更新为0.0.8

--- chrome/content/scripts/delitem.js
+++ chrome/content/scripts/delitem.js
@@ -36,21 +36,30 @@ Zotero.DelItem = {
                                 for (let id of attachmentIDs) { //4 for
                                     let attachment = Zotero.Items.get(id);
                                     let ifLinks = (attachment.attachmentLinkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE); // 检测是否为链接模式
-                                // if (attachment.attachmentContentType == 'text/html' ) { //可以筛选删除的附件类型
-                                        attachment.deleted = true; //删除附件(快照)
+                                    var file = await attachment.getFilePathAsync();
+                                    if (file && ifLinks) { // 如果文件存在(文件可能已经被删除)且为链接模式删除文件
+                                        try { await OS.File.remove (file); // 尝试删除文件
+                                        } catch (error) { // 弹出错误
+                                          alert ("The file can not be deleted. Please close the file if you have opened it and try again.");
+                                          return; // 弹出错误后终止执行       
+                                        }
+                                        }  
+                                // if (attachment.attachmentContentType == 'text/html' ) { // 可以筛选删除的附件类型
+                                        attachment.deleted = true; // 删除附件(快照)
                                         await attachment.saveTx();   
                                 // }
-                                    var file = await attachment.getFilePathAsync();
-                                    if (file && ifLinks) { //如果文件存在(文件可能已经被删除)且为链接模式删除文件
-                                        await OS.File.remove(file); //删除文件 
-                                        }  
+                          
                                 } //4 for
                             } // 3 if
                         if (item.isAttachment()) { //附件条目 5 if
                             var ifLinksAtt = (item.attachmentLinkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE); //检测是否为链接模式
                             var file = await item.getFilePathAsync();
                             if (file && ifLinksAtt) { // 如果文件存在(文件可能已经被删除)且为链接模式删除文件
-                                await OS.File.remove(file); //删除文件 
+                                try { await OS.File.remove (file); // 尝试删除文件
+                                } catch (error) { // 弹出错误
+                                  alert ("The file can not be deleted. Please close the file if you have opened it and try again.");
+                                  return; // 弹出错误后终止执行       
+                                }   
                                 }  
                             item.deleted = true; 
                             await item.saveTx();
@@ -59,6 +68,7 @@ Zotero.DelItem = {
             }
         } 
     },
+
 
     DelSnap: async function () { // 仅快照时调用的函数
 
@@ -100,51 +110,6 @@ Zotero.DelItem = {
         } 
     },
 
-    /*
-    DelItems: async function (items) { //删除条目和附件时被调用的执行具体删除任务的函数
-        Components.utils.import("resource://gre/modules/osfile.jsm");
-        var zfPath = Zotero.ZotFile.getPref("dest_dir");   //得到zotfile路径
- 
-        for (let item of items) {  // 1 for
-            title = item.getField('title');
-
-            file = await getFilePath(item); //调用函数
-            if (file){
-                
-                var filePath = OS.Path.dirname(file); //得到文件存放的文件夹
-                if (filePath != zfPath){ //如果两个文件夹不一致，文件可能存在storage中
-                    await OS.File.removeDir(filePath) //删除文件夹
-                    }
-                await OS.File.remove(file); //删除文件
-            }
-            item.deleted = true; //删除条目
-            await item.saveTx();
-
-        }// 1 for
-
-      
-
-        async function getFilePath(item) { //1 函数 得到文件路径
-
-            if (item && !item.isNote()) { //2 if
-
-                    if (item.isRegularItem()) { // Regular Item 一般条目//3 if 
-                    
-                        let attachmentIDs = item.getAttachments();
-                        for (let id of attachmentIDs) { //4 for
-                            var file = await Zotero.Items.get(id).getFilePathAsync();
-                            return file;
-                        } //4 for
-                    } // 3 if
-                    if (item.isAttachment()) { //附件条目 5 if
-                            var file = await item.getFilePathAsync();
-                            return file;
-                    }//5if
-            } //2 if
-
-        } ;
-    },
-    */
 
     DelItems: async function (items) { //删除条目和附件时被调用的执行具体删除任务的函数
       
@@ -156,14 +121,19 @@ Zotero.DelItem = {
                                 for (let id of attachmentIDs) { //4 for
                                     let attachment = Zotero.Items.get(id);
                                     let ifLinks = (attachment.attachmentLinkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE); // 检测是否为链接模式
+                                    var file = await attachment.getFilePathAsync();
+                                    if (file && ifLinks) { //如果文件存在(文件可能已经被删除)且为链接模式删除文件
+                                        try { await OS.File.remove (file); // 尝试删除文件
+                                        } catch (error) { // 弹出错误
+                                          alert ("The file can not be deleted. Please close the file if you have opened it and try again.");
+                                          return; // 弹出错误后终止执行       
+                                        }
+                                        }  
                                 // if (attachment.attachmentContentType == 'text/html' ) { //可以筛选删除的附件类型
                                         attachment.deleted = true; //删除附件(快照)
                                         await attachment.saveTx();   
                                 // }
-                                    var file = await attachment.getFilePathAsync();
-                                    if (file && ifLinks) { //如果文件存在(文件可能已经被删除)且为链接模式删除文件
-                                        await OS.File.remove(file); //删除文件 
-                                        }  
+                                  
                                 } //4 for
                             
                             } // 3 if
@@ -171,7 +141,11 @@ Zotero.DelItem = {
                             var ifLinksAtt = (item.attachmentLinkMode == Zotero.Attachments.LINK_MODE_LINKED_FILE); //检测是否为链接模式
                             var file = await item.getFilePathAsync();
                             if (file && ifLinksAtt) { // 如果文件存在(文件可能已经被删除)且为链接模式删除文件
-                                await OS.File.remove(file); //删除文件 
+                                try { await OS.File.remove (file); // 尝试删除文件
+                                } catch (error) { // 弹出错误
+                                  alert ("The file can not be deleted. Please close the file if you have opened it and try again.");
+                                  return; // 弹出错误后终止执行       
+                                }  
                                 }  
 
                             }//5if
@@ -182,7 +156,7 @@ Zotero.DelItem = {
         
     },
 
-
+ 
     // 检查附件是否存在函数
     checkItemAtt: function (item) { 
         if (item && !item.isNote()) {
